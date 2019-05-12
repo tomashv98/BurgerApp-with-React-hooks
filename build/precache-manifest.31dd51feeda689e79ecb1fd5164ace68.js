4303df7f17a7f8a227c542f39d5e022eb68a53aa
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2019-05-12 22:41:09 +0200

    Build

---
+++ build/precache-manifest.31dd51feeda689e79ecb1fd5164ace68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6374c1300b3936d2b48c",
+    "url": "/static/js/main.d43083fb.chunk.js"
+  },
+  {
+    "revision": "5aba1a87f050b920dda6",
+    "url": "/static/js/2.31d49fb8.chunk.js"
+  },
+  {
+    "revision": "6374c1300b3936d2b48c",
+    "url": "/static/css/main.c6101897.chunk.css"
+  },
+  {
+    "revision": "fe2477a4a59fee5e2ed44f1c1e7e3617",
+    "url": "/index.html"
+  }
+];
